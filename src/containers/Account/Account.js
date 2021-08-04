dc1e0395e8e6f589c34d87ffccf375de417034ed
nikolaabadic <nikolaabadic@gmail.com>
2021-08-04 13:47:57 +0200

    Inputs and outputs to components updated

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -1,15 +1,37 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
+import {connect} from 'react-redux'
 import AccountInfo from '../../components/AccountInfo/AccountInfo'
 import Payments from '../../components/Payments/Payments'
+import * as actions from '../../store/actions/actions'
 
-const account = () => {
-    return(
-        <Fragment>
-            <AccountInfo
-                currency="RSD" type="Current" number="1000-2000-3000" amount="0"/>
-            <Payments/>
-        </Fragment>
-    )
+class Account extends Component {
+    componentDidMount(){
+        this.props.onGetAllPayments()
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <AccountInfo
+                    currency="RSD" type="Current" number="1000-2000-3000" amount={this.props.amount}/>
+                <Payments payments={this.props.payments}/>
+            </Fragment>
+        )
+    }
 }
 
-export default account
+const mapStateToProps = state => {
+    return{
+        payments: state.payments,
+        amount: state.amount
+    }
+}
+
+const mapDispatchToProps = dispatch =>{
+    return{
+        onGetAllPayments: () => dispatch(actions.getPayments()),
+        onGetAmount: () => dispatch(actions.getAmount())
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Account)
